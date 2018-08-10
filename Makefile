8264fbe2292e6ede944141d4de548df9cd18f1b1
Léo Peltier <contact@leo-peltier.fr>
2018-08-10 23:03:17 +0200

    Add pprof support

--- Makefile
+++ Makefile
@@ -3,6 +3,11 @@ all: poussin
 poussin:
 	go build
 
-.PHONY: poussin run
+.PHONY: poussin run pprof
 run: poussin
-	./poussin
+	./poussin boot.gb rom.gb
+
+pprof: poussin
+	./poussin -cpuprofile cpu.pprof -memprofile mem.pprof boot.gb rom.gb
+	go-torch --file="/tmp/torch.svg" -b cpu.pprof
+	sensible-browser "/tmp/torch.svg"
--- main.go
+++ main.go
@@ -1,18 +1,37 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"image"
 	"io/ioutil"
+	"log"
 	"os"
+	"runtime"
+	"runtime/pprof"
 
 	"home.leo-peltier.fr/poussin/emu"
 	"home.leo-peltier.fr/poussin/renderer/gl"
 )
 
 func main() {
-	if len(os.Args) != 3 {
-		fmt.Println("Usage: poussin BOOTROM ROM")
+	var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+	var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
+	flag.Parse()
+
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
+	if len(flag.Args()) != 2 {
+		fmt.Println("Usage: poussin [-cpuprofile FILE] [-memprofile FILE] BOOTROM ROM")
 		os.Exit(1)
 	}
 
@@ -20,7 +39,7 @@ func main() {
 	gb := emu.NewGameboy(nextFrame)
 	defer gb.Close()
 
-	bootRom, err := ioutil.ReadFile(os.Args[1])
+	bootRom, err := ioutil.ReadFile(flag.Args()[0])
 	if err != nil {
 		panic(err)
 	}
@@ -28,7 +47,7 @@ func main() {
 		panic(err)
 	}
 
-	rom, err := ioutil.ReadFile(os.Args[2])
+	rom, err := ioutil.ReadFile(flag.Args()[1])
 	if err != nil {
 		panic(err)
 	}
@@ -42,5 +61,18 @@ func main() {
 
 	select {
 	case <-quit:
+
+	}
+
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		runtime.GC() // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
+		f.Close()
 	}
 }
